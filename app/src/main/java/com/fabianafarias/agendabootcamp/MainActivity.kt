2a2948ea9aa260e1c8f3d01924ca4fe0bc8c91ed
fabiana farias <farias.fabianam@gmail.com>
2021-05-17 16:46:28 -0300

    Desenvolvendo a classe MainActivity

--- app/src/main/java/com/fabianafarias/agendabootcamp/MainActivity.kt
+++ app/src/main/java/com/fabianafarias/agendabootcamp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.fabianafarias.agendabootcamp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Events.*
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +15,14 @@ class MainActivity : AppCompatActivity() {
         val btnSetEvent = findViewById(R.id.set_event) as Button
         btnSetEvent.setOnClickListener {
 
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CONTENT_URI)
+                .putExtra(TITLE, "Bootcamp everis")
+                .putExtra(EVENT_LOCATION, "on line")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis() + (60*60*1000))
+
+            startActivity(intent)
         }
     }
 }
