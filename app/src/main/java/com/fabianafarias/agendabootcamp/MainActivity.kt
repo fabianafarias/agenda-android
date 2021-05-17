0934a8f7c0b84839fe73b59826d19e78455b8219
fabiana farias <farias.fabianam@gmail.com>
2021-05-17 16:35:41 -0300

    Preparando para inserir evento na agenda

---
+++ app/src/main/java/com/fabianafarias/agendabootcamp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.fabianafarias.agendabootcamp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnSetEvent = findViewById(R.id.set_event) as Button
+        btnSetEvent.setOnClickListener {
+
+        }
+    }
+}
